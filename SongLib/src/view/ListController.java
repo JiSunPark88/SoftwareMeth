1bd7329fcc08e80eab8bc90624b281513a49b8fe
JiSunPark88 <47510681+JiSunPark88@users.noreply.github.com>
2019-10-02 16:40:03 -0400

    Update ListController.java

--- SongLib/src/view/ListController.java
+++ SongLib/src/view/ListController.java
@@ -1,63 +1,58 @@
 package view;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import model.SongList;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 import model.CompareSong;
+import model.SongList;
+
 
 
 public class ListController {
 	
+	ListView<SongList> ListView;
 	
-/*read song from the txt file
- list alphabetical order*/
-	 private ArrayList<SongList> readFile(String filePathName)
-	   {
-		   ArrayList <SongList> songLists = new ArrayList<SongList>();
-		   BufferedReader buffReader;
-		   Path file = Paths.get(filePathName);
-		   try {
+	Label displayName, displayArtist, displayAlbum, displayYear; //variables to display string
+	TextField Name, Artist, Album, Year; //variables used when songs are added, deleted, edited
+	
+	private ObservableList<SongList> list = FXCollections.observableArrayList();
+	
+	String fileName = "src/songfile.txt";
 
-				if (!new File(filePathName).exists())
-				{
-				   return songLists;
-				}
-			buffReader = Files.newBufferedReader(file);
-			   String textFile = buffReader.readLine();
-				
-			   while (textFile != null) { 
-		              
-				   String name = textFile;
-		               
-				   textFile = buffReader.readLine();
-				   String artist = textFile;
-		               
-				   textFile = buffReader.readLine();
-				   String album = textFile;
-		               
-				   textFile = buffReader.readLine();
-				   String year = textFile;
-		               
-				   SongList temp = new SongList(name, artist, album, year);
-				   songLists.add(temp);
-		               
-				   textFile = buffReader.readLine(); 
-			   }
-			  
-				
-		   } catch (IOException e) {
-			   e.printStackTrace();
-		   }
-		      
-		   Collections.sort(songLists, new CompareSong());
-		   return songLists;
-	   }
+	
+	//display song
+	public void displaySong() {
+		if(list.isEmpty()) {
+			displayName.setText("");
+			displayArtist.setText("");
+			displayAlbum.setText("");
+			displayYear.setText("");
+		}
+		
+		SongList song = ListView.getSelectionModel().getSelectedItem();  
+		
+		if(song!=null) {
+			displayName.setText(song.getName());
+			displayArtist.setText(song.getArtist());
+			displayAlbum.setText(song.getAlbum());
+			displayYear.setText(song.getYear());
+		}
+	}// end displaySong
+
+	
 
 }
