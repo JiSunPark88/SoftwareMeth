bfd3edd8ef87ad0c03d768aa52b6bd47e7f70636
JiSunPark88 <47510681+JiSunPark88@users.noreply.github.com>
2019-10-03 16:24:16 -0400

    Update SongLib.java

--- SongLib/src/application/SongLib.java
+++ SongLib/src/application/SongLib.java
@@ -1,9 +1,33 @@
 package application;
 
-public class SongLib {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import view.ListController;
 
-  
-  	public static void main(String[] args) {
+public class SongLib extends Application {
+	public void start (Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/view/songLib.fxml"));
+			GridPane root = loader.load();
+			
+			ListController listController = loader.getController();
+			listController.start(primaryStage); 
+			
+			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
+			primaryStage.setTitle("Song Library by jp1476 and mms402");
+			primaryStage.setScene(scene);
+			primaryStage.setResizable(true);
+			primaryStage.show();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
