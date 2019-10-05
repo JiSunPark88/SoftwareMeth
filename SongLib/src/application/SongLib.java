ffd6ecbdc9fc41fb42792049c22ec083d5762ae4
JiSunPark88 <47510681+JiSunPark88@users.noreply.github.com>
2019-10-04 21:57:11 -0400

    Update SongLib.java

--- SongLib/src/application/SongLib.java
+++ SongLib/src/application/SongLib.java
@@ -1,29 +1,39 @@
+//SongLib Project CS213 Project 1
+//Ji Sun Park and Sakar Mohamed M
+
 package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import view.ListController;
 
-public class SongLib extends Application {
-	public void start (Stage primaryStage) throws Exception {
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(getClass().getResource("/view/songLib.fxml"));
-			GridPane root = loader.load();
-			
-			ListController listController = loader.getController();
-			listController.start(primaryStage); 
-			
-			Scene scene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
-			primaryStage.setTitle("Song Library by jp1476 and mms402");
-			primaryStage.setScene(scene);
-			primaryStage.setResizable(true);
-			primaryStage.show();
-	}
+public class SongLib extends Application{
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(
+				getClass().getResource("/view/SongLib.fxml")); 
+		
+		AnchorPane root = (AnchorPane)loader.load();
+		
+		ListController controller = loader.getController();
+		controller.start(primaryStage);
+		
+		Scene scene = new Scene(root);
+	    primaryStage.setTitle("Song Library - jp1476 and mms402 ");
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+	
 	public static void main(String[] args) {
+		
 		launch(args);
 	}
+
 }
