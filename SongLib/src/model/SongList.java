ec8974b813f83a06fe2a8bc8edcf4b7f0a5b70c1
JiSunPark88 <47510681+JiSunPark88@users.noreply.github.com>
2019-10-04 21:57:48 -0400

    Update SongList.java

--- SongLib/src/model/SongList.java
+++ SongLib/src/model/SongList.java
@@ -1,49 +1,74 @@
+//SongLib Project CS213 Project 1
+//Ji Sun Park and Sakar Mohamed M
+
 package model;
 
 public class SongList {
 	
-private String name, artist, album, year;
+	private String name, artist, album, year;
 	
-	public SongList (String name, String artist, String album, String year) {
-		this.name=name;
-		this.artist=artist;
-		this.album=album;
-		this.year=year;
+	public SongList(String name, String artist) { 
+		
+		this(name, artist, "", "");
 	}
 	
-	public String getName() {
+	public SongList(String name, String artist,  String album) { 
+		
+		this(name, artist, album, "");
+	}
+	
+	public SongList(String name, String artist, String album, String year) { 
+		
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+	
+	public String getName() {  
+		
 		return this.name;
-	}
-	public void setName(String name) {
-		this.name=name;
 	}
 	
 	public String getArtist() {
+		
 		return this.artist;
 	}
-	public void setArtist(String artist) {
-		this.artist=artist;
-	}
 	
-	public String getAlbum() {
+	public String getAlbum() {  
+		
 		return this.album;
 	}
-	public void setAlbum(String album) {
-		this.album=album;
-	}
 	
-	public String getYear() {
+	public String getYear() {  
+		
 		return this.year;
 	}
-	public void setYear(String year) {
-		this.year=year;
+	
+	public void setTitle(String name) {  
+		
+		this.name = name;
 	}
 	
-	public String displaySong () {
-		return this.name + "by" + this.artist;
+	public void setArtist(String artist) {  
+		
+		this.artist = artist;
 	}
-	public String detailedSong () {
-		return this.name + "\n" + this.artist + "\n" + this.album + "\n" + this.year + "\n";
+	
+	public void setAlbum(String album) {  
+		
+		this.album = album;
+	}
+	
+	public void setYear(String year) {  
+		
+		this.year = year;
+	}
+	
+	
+	public String toString() {
+		
+		return name + " - " + artist;
 	}
 
 }
