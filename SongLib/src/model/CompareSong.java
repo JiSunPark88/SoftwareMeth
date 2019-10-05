e49d5380afd7a9d06a3d217824fcce7a8c5ecdda
JiSunPark88 <47510681+JiSunPark88@users.noreply.github.com>
2019-10-04 21:58:10 -0400

    Update CompareSong.java

--- SongLib/src/model/CompareSong.java
+++ SongLib/src/model/CompareSong.java
@@ -1,20 +1,21 @@
+//SongLib Project CS213 Project 1
+//Ji Sun Park and Sakar Mohamed M
+
 package model;
 
 import java.util.Comparator;
 
-public class CompareSong implements Comparator <SongList> {
-	
+public class CompareSong implements Comparator<SongList>{
+
+	@Override
 	public int compare(SongList first, SongList second) {
+		
+		if(first.getName().toLowerCase().compareTo(second.getName().toLowerCase()) == 0) {
+			
+			return first.getArtist().toLowerCase().compareTo(second.getArtist().toLowerCase());
+		}
+		
 		return first.getName().toLowerCase().compareTo(second.getName().toLowerCase());
 	}
-	
-	public class IgnoreSensitive implements Comparator<Object> {
-	    public int compare(Object o1, Object o2) {
-	        String frist = (String) o1;
-	        String second = (String) o2;
-	        return frist.toLowerCase().compareTo(second.toLowerCase());
-	    }
 
-}
-	
 }
